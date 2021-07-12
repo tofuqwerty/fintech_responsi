6d7b41185637e440ba93c87a5bf5bad17874cf1b
tofuqwerty <hanif.tofa@gmail.com>
2021-07-12 17:49:29 +0700

    fintech responsi

---
+++ app/src/main/java/io/github/tofuqwerty/fintechresponsi/Profile.kt
@@ -0,0 +1,11 @@
+package io.github.tofuqwerty.fintechresponsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ app/src/main/java/io/github/tofuqwerty/fintechresponsi/Recovery.kt
@@ -0,0 +1,11 @@
+package io.github.tofuqwerty.fintechresponsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recovery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery)
+    }
+}
